c906a4827c8ef4e57528dd28d88d677cb39eb1c6
Øystein Hukset <oystein.hukset@redink.no>
2019-02-25 12:04:10 +0100

    Instal prompt

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ if ('serviceWorker' in navigator) {
 }
 
 // Promt Android users for install
-let deferredPrompt;
+// let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    // showAddToHomeScreen();
-    console.log('Add');
-});
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     e.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//     // Update UI notify the user they can add to home screen
+//     // showAddToHomeScreen();
+//     console.log('Add');
+// });
 
-function showAddToHomeScreen() {
-    var btnAdd = document.querySelector(".btnAdd");
-    btnAdd.style.display = "block";
+// function showAddToHomeScreen() {
+//     var btnAdd = document.querySelector(".btnAdd");
+//     btnAdd.style.display = "block";
 
-}
+// }
